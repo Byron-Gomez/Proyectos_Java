88a3d5a725b6309c5a7823e924288343817f491e
byronxgomez <byronxgomez127@gmail.com>
2024-09-23 23:02:28 -0600

    proyectos java

--- src/main/java/org/byronxgomez/tiendalibros/TiendaDeLibros.java
+++ src/main/java/org/byronxgomez/tiendalibros/TiendaDeLibros.java
@@ -5,26 +5,21 @@ import java.util.Scanner;
 public class TiendaDeLibros {
     public static void main(String[] args) {
 
-        // creamos un objeto de la clase scaner para leer lo que ingrese el usuario
+
         Scanner scanner = new Scanner(System.in);
 
-        // se pide el ID y se captura
         System.out.println("Proporciona el ID : ");
-        int id = Integer.parseInt(scanner.nextLine());  // Leemos la entrada como cadena y luego la convertimos a entero con Integer.parseInt()
+        int id = Integer.parseInt(scanner.nextLine());
 
-        // se pide el nombre y se captura
         System.out.println("Proporcionar Nombre : ");
-        String nombre = scanner.nextLine();  // Leemos el nombre del libro directamente como cadena de texto (String)
+        String nombre = scanner.nextLine();
 
-        // se pide el precio y se captura
         System.out.println("Proporcionar el Precio : ");
-        double precio = Double.parseDouble(scanner.nextLine());  // Leemos la entrada como cadena y luego la convertimos a un número decimal (double)
+        double precio = Double.parseDouble(scanner.nextLine());
 
-        // se pide si es gratis o no el envio y se captura
         System.out.println("Proporcionar el envio Gratuito (true / false) : ");
-        boolean envioGratuito = Boolean.parseBoolean(scanner.nextLine());  // Leemos la entrada como cadena y la convertimos a booleano con Boolean.parseBoolean()
+        boolean envioGratuito = Boolean.parseBoolean(scanner.nextLine());
 
-        // se imprimen los datos en formato que se pidio
         System.out.println("---------------------------------------------------------------");
         System.out.println(nombre + " #" + id);
         System.out.println("Precio: $" + precio);
